33a19444da7dcb2d7eae10977a410d919a11e75f
Stepan Rudiak <Stepan.rudiak95@gmail.com>
2023-12-05 13:36:25 +0100

    mongo db connected

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO_KEY)
+  .then(() => {
+    console.log('Connected to mongoDb')
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
 
 const app = express();
 
@@ -6,5 +19,5 @@ const app = express();
 
 
 app.listen(3000, () => {
-  console.log('sever listening on port 3000 111')
+  console.log('sever listening on port 3000')
 })
